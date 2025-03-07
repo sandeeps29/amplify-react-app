77755daa035459188e5ee0f3358d11d4ab5d2c36
root <root@com038.>
2025-03-07 04:46:07 +0000

    second commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello from AWS Amplify + Terraform!</h1>
+        <p>Edit <code>src/App.js</code> and push changes to GitHub to see updates in Amplify.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
